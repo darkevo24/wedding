ceafadf535a8b698fa17319280dd6ce1c77f3c71
darkevo2424 <timotius.albert@luxpmsoft.com>
2022-03-16 12:51:41 +0700

    timo

--- src/components/layouts/pages/home/vendorSearch.js
+++ src/components/layouts/pages/home/vendorSearch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HiOutlineArrowRight } from "react-icons/hi";
 import Image from "next/image";
 import Banner1 from "../../../../../public/images/vendor-search/banner1.png";
@@ -13,10 +13,10 @@ const VendorSearch = () => {
       <div className="w-full flex flex-col items-center">
         <p className="text-3xl">Your Vendor Search - Streamlined</p>
         <p>Pre-screened vendors ready to help your planning</p>
-        <div className="flex mt-7">
+        <div className="flex flex-col lg:flex-row mt-7 justify-center">
           <div data-aos="fade-right">
-            <div className="mx-3 cursor-pointer transition-all hover:scale-105">
-              <div className="h-80 w-72 rounded-t-md relative">
+            <div className="mx-3 cursor-pointer transition-all hover:scale-105 max-w-screen my-2 lg:my-0">
+              <div className="h-80 w-72 rounded-t-md relative ">
                 <Image src={Banner1} layout={"fill"} />
               </div>
               <div className="w-72 bg-bg-primary hover:bg-bg-primary-darker flex justify-center items-center py-2 space-x-2">
@@ -26,7 +26,7 @@ const VendorSearch = () => {
             </div>
           </div>
           <div data-aos="fade-right">
-            <div className="mx-3 cursor-pointer transition-all hover:scale-105">
+            <div className="mx-3 cursor-pointer transition-all hover:scale-105 my-2 lg:my-0">
               <div className="h-80 w-72 rounded-t-md relative">
                 <Image src={Banner2} layout={"fill"} />
               </div>
@@ -53,13 +53,13 @@ const VendorSearch = () => {
             </div>
             <div className="relative rounded-md h-24 w-80 cursor-pointer transition-all hover:scale-105">
               <Image src={Banner5} layout={"fill"} className="rounded-md" />
-              <div className="flex justify-center items-center space-x-2 absolute right-2 bottom-2 transition-all hover:right-1">
+              <div className="bottom-12 flex justify-center items-center space-x-2 absolute right-2 bottom-2 transition-all hover:right-1">
                 <p>Cakes & Confectionary</p>
                 <HiOutlineArrowRight size={20} />
               </div>
             </div>
             <div className="w-full flex justify-end">
-              <div className="flex justify-end items-center mt-2 space-x-2 transition-all hover:scale-110 cursor-pointer select-none">
+              <div className="bottom-14 relative flex justify-end items-center mt-2 space-x-2 transition-all hover:scale-110 cursor-pointer select-none">
                 <p>Explore more vendors</p>
                 <HiOutlineArrowRight size={20} />
               </div>
