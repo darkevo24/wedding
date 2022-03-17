74f30c51f61b06123b5667cb916e7eb3346a44cf
darkevo2424 <timotius.albert@luxpmsoft.com>
2022-03-17 14:32:20 +0700

    FAQS

--- src/components/layouts/pages/register/faqVendor.js
+++ src/components/layouts/pages/register/faqVendor.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const FaqVendor = () => {
   return (
-    <div className="w-full bg-bg-serenade px-10 py-10">
+    <div className="w-full bg-bg-serenade px-10 py-10 text-bg-primary">
       <p className="text-2xl">FAQs</p>
+      <br></br>
+      <div className="w-full h-0.5 bg-bg-primary">
+      </div>
+      <br></br>
     </div>
   );
 };
