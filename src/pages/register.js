645755424e98e2c3b6ff31252aab5cdeb852d532
Tumas <putupadang13@gmail.com>
2022-03-17 17:03:42 +0800

    update

--- src/pages/register.js
+++ src/pages/register.js
@@ -21,7 +21,129 @@ const Register = () => {
         </div>
         <form>
           <div className="mt-5 w-full">
-            <p>add form here</p>
+            <div className="flex space-x-2">
+              <div className="w-1/2">
+                <div>
+                  <p>BRIDE'S NAME *</p>
+                  <div className="flex space-x-2">
+                    <input type="text" className="w-full" />
+                    <input type="text" className="w-full" />
+                  </div>
+                </div>
+                <div>
+                  <p>EMAIL ADDRESS *</p>
+                  <input type="text" className="w-full" />
+                </div>
+              </div>
+              <div className="w-1/2">
+                <div>
+                  <p>BRIDE'S NAME *</p>
+                  <div className="flex space-x-2">
+                    <input type="text" className="w-full" />
+                    <input type="text" className="w-full" />
+                  </div>
+                </div>
+                <div>
+                  <p>WEDDING DATE *</p>
+                  <input type="text" className="w-full" />
+                </div>
+              </div>
+            </div>
+            <div>
+              <p>HOW DID YOU HEAR ABOUT US?</p>
+              <div className="flex justify-between">
+                <div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="billboard" />
+                    <label for="billboard" className="ml-2">
+                      MRT/Billboard
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="tv" />
+                    <label for="tv" className="ml-2">
+                      TV
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="google" />
+                    <label for="google" className="ml-2">
+                      Google
+                    </label>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="instagram" />
+                    <label for="instagram" className="ml-2">
+                      Instagram
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="facebook" />
+                    <label for="facebook" className="ml-2">
+                      Facebook
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="pinterest" />
+                    <label for="pinterest" className="ml-2">
+                      Pinterest
+                    </label>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="friend" />
+                    <label for="friend" className="ml-2">
+                      Friend or Family
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="tiktok" />
+                    <label for="tiktok" className="ml-2">
+                      Tik Tok
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="podcast" />
+                    <label for="podcast" className="ml-2">
+                      Podcast
+                    </label>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center">
+                    <input type="checkbox" id="other" />
+                    <label for="other" className="ml-2">
+                      Other
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p>Almost There!</p>
+              <div className="flex">
+                <p>Create an account, so we can save your preferences.</p>&nbsp;
+                <p>
+                  Already have an account? <span className="cursor-pointer hover:underline">Login.</span>
+                </p>
+              </div>
+              <div>
+                <div className="flex">
+                  <p>Email Address</p>
+                  <input type="text" name="" id="" />
+                </div>
+                <div className="flex">
+                  <p>Password</p>
+                  <input type="password" name="" id="" />
+                </div>
+              </div>
+            </div>
+            <button type="submit" className="bg-bg-primary hover:bg-bg-dark-grey px-7 py-2 mt-5 rounded-md text-white">
+              Submit
+            </button>
           </div>
         </form>
       </div>
