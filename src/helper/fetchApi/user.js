531f1534b2066e900c72a2a6d7053b7dec095769
Tumas <putupadang13@gmail.com>
2022-03-18 20:14:57 +0800

    update

--- src/helper/fetchApi/user.js
+++ src/helper/fetchApi/user.js
@@ -1,40 +1,4 @@
-export const fetchApiLogin = (params, res) => {
-  fetch(process.env.API_URL + "customer/auth/register", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(params),
-  })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      if (responseJson.hasOwnProperty("success") && responseJson.success == true) {
-        res({ resVal: responseJson });
-      } else {
-        res(JSON.stringify(responseJson.message));
-      }
-    });
-};
-
-export const fetchApiGetUser = (params, res) => {
-  fetch(process.env.API_URL + "customer/auth/register", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(params),
-  })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      if (responseJson.hasOwnProperty("success") && responseJson.success == true) {
-        res({ resVal: responseJson });
-      } else {
-        res(JSON.stringify(responseJson.message));
-      }
-    });
-};
+import { tokenDecrypt } from "../tokenHash";
 
 export const fetchApiRegister = (params, res) => {
   fetch(process.env.API_URL + "customer/auth/register", {
@@ -44,6 +8,47 @@ export const fetchApiRegister = (params, res) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(params),
+  })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if (responseJson.hasOwnProperty("success") && responseJson.success == true) {
+        res({ resVal: responseJson });
+      } else {
+        res(JSON.stringify(responseJson.message));
+      }
+    });
+};
+
+export const fetchApiLogin = (params, res) => {
+  fetch(process.env.API_URL + "customer/auth/login", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if (responseJson.hasOwnProperty("success") && responseJson.success == true) {
+        res({ resVal: responseJson });
+      } else {
+        res(JSON.stringify(responseJson.message));
+      }
+    });
+};
+
+export const fetchApiGetUser = (res) => {
+  let token = localStorage.getItem("token");
+  token = tokenDecrypt(token);
+
+  fetch(process.env.API_URL + "customer/auth/me", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
   })
     .then((response) => response.json())
     .then((responseJson) => {
